d0ded6dbe4ec2031c2cf03c18f17e941b5a27623
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-19 22:29:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+class Dog 
+  
+  @@all = []
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+    self.save
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.clear_all
+    @@all = []
+  end
+  
+  def self.print_all
+    names = []
+    @@all.each do 
+      names << "#{:name}" 
+    end
+    names 
+  end
+  
+  def save 
+    @@all << self
+  end
+  
+end
